29853f76e9c80c4ec5f69943d695c344fdc2dee2
Lidia Gonzalez <lidia_1508@hotmail.com>
2023-10-02 14:23:07 +0100

    Iteration 3 done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+
 function HomePage() {
-    return (
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchAllCountries = async () => {
+    const response = await fetch(
+      "https://ih-countries-api.herokuapp.com/countries"
+    );
+    if (response.ok) {
+      const parsed = await response.json();
+      setCountries(parsed);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllCountries();
+  }, []);
+
+  
+
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
     <>
     <h1>WikiCountries: Your Guide to the World</h1>
-
+    <ul>
+        {countries.map(currentCountry => (
+            <li key={currentCountry._id}>
+                <h4>{currentCountry.name.official}</h4>
+            </li>
+        ))}
+    </ul>
     </>
-    ) 
+  )
 }
 
 export default HomePage;
